87d55612c92dc5fc39f96855fae2634ac1b7cd7c
Alessandro Iannacone Romero <alex1400ahjulio@gmail.com>
2025-11-02 14:08:32 -0500

    Añadimos PedidoController y PedidoView

--- Facade_Adapter_/src/controller/PedidoController.java
+++ Facade_Adapter_/src/controller/PedidoController.java
@@ -9,5 +9,12 @@ import model.PedidoResultado;
 import view.PedidoView;
 
 public class PedidoController {
+    private PedidoFacade facade;
+    private PedidoView view;
     
+    public PedidoController(PedidoFacade facade, PedidoView view){
+        this.facade = facade;
+        this.view = view;
+    }
+    public void registrarPedido(String cliente, String producto, int cantidad, double)
 }
--- Facade_Adapter_/src/view/PedidoView.java
+++ Facade_Adapter_/src/view/PedidoView.java
@@ -3,11 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package view;
+import model.PedidoResultado;
 
-/**
- *
- * @author PC
- */
+
+
 public class PedidoView {
-    
+    public void mostrarComprobante(PedidoResultado r) {
+        System.out.println("\n=== COMPROBANTE ===");
+        System.out.println("Cliente : " + r.cliente);
+        System.out.println("Producto: " + r.producto);
+        System.out.println("Cantidad: " + r.cantidad);
+        System.out.printf("Subtotal: S/%.2f%n", r.subtotal);
+        System.out.printf("IGV (18%%): S/%.2f%n", r.igv);
+        System.out.printf("Total: S/%.2f%n", r.total);
+        System.out.println("====================\n");
+    }
+
+    public void mostrarError(String mensaje) {
+        System.out.println("Error: " + mensaje + "\n");
+    }
 }
+
+
