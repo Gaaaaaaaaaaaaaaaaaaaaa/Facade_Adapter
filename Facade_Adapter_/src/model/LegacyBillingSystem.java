f548ebf77d1d0b1a8ab5d0cb22cf267db2c1bf53
Alessandro Iannacone Romero <alex1400ahjulio@gmail.com>
2025-11-02 13:38:09 -0500

    Aqui añadimos el LegacyBillingSystem
    Aqui generamos la factura antigua donde nos pide el caso

---
+++ Facade_Adapter_/src/model/LegacyBillingSystem.java
@@ -0,0 +1,9 @@
+
+package model;
+
+
+public class LegacyBillingSystem {
+    public void generarFacturaAntigua(String nombreCliente, double montoTotal) {
+        System.out.println("[Legacy] Factura generada para " + nombreCliente + " por S/" + montoTotal);
+    }
+}
