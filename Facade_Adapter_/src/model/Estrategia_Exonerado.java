9548edde206af7d486045154205ca78e64df728d
Alessandro Iannacone Romero <alex1400ahjulio@gmail.com>
2025-11-07 08:43:11 -0500

    TAREA 02: Añadimos la clase Estrategia_Exonerado
    Aqui tmb implementa de esa interface pero esta vez hace una sobreescritura y retorna 0;

---
+++ Facade_Adapter_/src/model/Estrategia_Exonerado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+
+
+public class Estrategia_Exonerado implements Estrategia_Impuesto {
+    @Override
+    public double calcular(double subtotal) {
+        return 0;
+    }
+}
+
